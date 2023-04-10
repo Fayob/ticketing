3e7bbb4e4accadb72574a15d028e70597e2950c9
fayob <adedejifavour6@gmail.com>
2023-04-10 19:48:05 +0100

    Add sign up logic to app

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,31 +1,43 @@
 import express, { Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
-import { RequestValidationError } from '../errors/request-validation-error'
+import jwt from 'jsonwebtoken'
+
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('email must be valid'),
   body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
-  }
-  
+], validateRequest, async (req: Request, res: Response) => {
   const { email, password } = req.body
 
   // if (!email || typeof email !== 'string') {
   //   res.status(400).send('Provide a valid email address')
   // }
-  
-  console.log('Creating User');
-  throw new DatabaseConnectionError();
-  
 
-  res.send({})
+  const existingUser = await User.findOne({ email })
+
+  if (existingUser) {
+    throw new BadRequestError('Email already exists')
+  }
+  const user = User.build({ email, password })
+  await user.save();
+
+  // Generate JWT
+  const userJWT = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, process.env.JWT_KEY!)
+
+  // Store it on session object
+  req.session = {
+    jwt: userJWT
+  }
+
+  res.status(201).json(user);
 })
 
 export { router as signUpRouter }
